5f16ddf6e16d3f50fe1a76ea955f8aecf6e92240
Hammad <66684303+Mohamad-hammad@users.noreply.github.com>
2023-01-26 00:39:51 +0500

    Update UI.py

--- UI.py
+++ UI.py
@@ -25,6 +25,7 @@ root.grid_rowconfigure(6,weight=1)
 root.grid_rowconfigure(7,weight=1)
 root.grid_rowconfigure(8,weight=1)
 #fonts
+
 #balance label
 font_balance = customtkinter.CTkFont(family="Poppins Regular", size=15)
 label_balance = customtkinter.CTkLabel(master=root, text="Balance",anchor="w",font = font_balance)
@@ -35,7 +36,7 @@ font_totalBalance = customtkinter.CTkFont(family="Poppins Bold", size=25)
 label_totalBalance = customtkinter.CTkLabel(master=root, text="Rs. 11,500",font = font_totalBalance)
 label_totalBalance.grid(row=0, column=0,padx=20,pady=20,sticky="nw")
 
-
+#barchart
 f = Figure(figsize=(5,4), dpi=100)
 f.set_facecolor("black")
 ax = f.add_subplot(111)
@@ -79,65 +80,65 @@ label_totalLoan = customtkinter.CTkLabel(master=root, text="Rs. 11,500",font = f
 label_totalLoan.grid(row=4, column=0,padx=200,pady=20,sticky="nw")
 
 #Monthly expense limit
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=15)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Monthly Expense Limit:",anchor="nw",font = font_tobePaid)
-label_tobePaid.grid(row=2, column=1, sticky="nw")
+font_expenseLimit= customtkinter.CTkFont(family="Poppins Regular", size=15)
+label_expenseLimit = customtkinter.CTkLabel(master=root, text="Monthly Expense Limit:",anchor="nw",font = font_expenseLimit)
+label_expenseLimit.grid(row=2, column=1, sticky="nw")
 
 #mothly expense limit amount label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Bold", size=20)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Rs.10000",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=2, column=1,pady=20, sticky="nw")
+font_expenseLimitAmount= customtkinter.CTkFont(family="Poppins Bold", size=20)
+label_expenseLimitAmount = customtkinter.CTkLabel(master=root, text="Rs.10000",anchor="w",font = font_expenseLimitAmount)
+label_expenseLimitAmount.grid(row=2, column=1,pady=20, sticky="nw")
 
 #This month's expenses
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=15)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="This Month's Limit:",anchor="nw",font = font_tobePaid)
-label_tobePaid.grid(row=2, column=1,pady=60, sticky="nw")
+font_thisMonthLimit= customtkinter.CTkFont(family="Poppins Regular", size=15)
+label_thisMonthLimit = customtkinter.CTkLabel(master=root, text="This Month's Limit:",anchor="nw",font = font_thisMonthLimit)
+label_thisMonthLimit.grid(row=2, column=1,pady=60, sticky="nw")
 
 #This month's expenses
-font_tobePaid= customtkinter.CTkFont(family="Poppins Bold", size=20)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Rs.8500",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=2, column=1,pady=80, sticky="nw")
+font_thisMonthLimitAmount= customtkinter.CTkFont(family="Poppins Bold", size=20)
+label_thisMonthLimitAmoun = customtkinter.CTkLabel(master=root, text="Rs.8500",anchor="w",font = font_thisMonthLimitAmount)
+label_thisMonthLimitAmoun.grid(row=2, column=1,pady=80, sticky="nw")
 
 #Cash flow for december label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=15)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Cash flow for December",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1, sticky="nw")
+font_cashflowLabel= customtkinter.CTkFont(family="Poppins Regular", size=15)
+label_cashflowLabe = customtkinter.CTkLabel(master=root, text="Cash flow for December",anchor="w",font = font_cashflowLabel)
+label_cashflowLabe.grid(row=4, column=1, sticky="nw")
 
 #Earned label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=10)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="EARNED",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,pady=20, sticky="nw")
+font_earnedLabel= customtkinter.CTkFont(family="Poppins Regular", size=10)
+label_earnedLabel = customtkinter.CTkLabel(master=root, text="EARNED",anchor="w",font = font_earnedLabel)
+label_earnedLabel.grid(row=4, column=1,pady=20, sticky="nw")
 
 #Earned amount label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Bold", size=10)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Rs.5000",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,padx=150,pady=20, sticky="nw")
+font_amountEarned= customtkinter.CTkFont(family="Poppins Bold", size=10)
+label_amountEarned = customtkinter.CTkLabel(master=root, text="Rs.5000",anchor="w",font = font_amountEarned)
+label_amountEarned.grid(row=4, column=1,padx=150,pady=20, sticky="nw")
 
 #progess bar
 progressbar = customtkinter.CTkProgressBar(master=root)
 progressbar.grid(row=4, column=1,pady=40,sticky="nw")
 
 #SPENT label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=10)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="SPENT",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,pady=60, sticky="nw")
+font_spentLabel= customtkinter.CTkFont(family="Poppins Regular", size=10)
+label_spentLabel = customtkinter.CTkLabel(master=root, text="SPENT",anchor="w",font = font_spentLabel)
+label_spentLabel.grid(row=4, column=1,pady=60, sticky="nw")
 
 #SPENT amount label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Bold", size=10)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Rs.1500",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,padx=150,pady=60, sticky="nw")
+font_spentAmount= customtkinter.CTkFont(family="Poppins Bold", size=10)
+label_spentAmount = customtkinter.CTkLabel(master=root, text="Rs.1500",anchor="w",font = font_spentAmount)
+label_spentAmount.grid(row=4, column=1,padx=150,pady=60, sticky="nw")
 
 #progess bar
 progressbar = customtkinter.CTkProgressBar(master=root)
 progressbar.grid(row=4, column=1,pady=80,sticky="nw")
 
 #Remaining cashflow amount label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Bold", size=20)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="Rs.35000",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,padx=210,pady=45, sticky="nw")
+font_remainingAmount= customtkinter.CTkFont(family="Poppins Bold", size=20)
+label_remainingAmount = customtkinter.CTkLabel(master=root, text="Rs.35000",anchor="w",font = font_remainingAmount)
+label_remainingAmount.grid(row=4, column=1,padx=210,pady=45, sticky="nw")
 
 #Remaning label
-font_tobePaid= customtkinter.CTkFont(family="Poppins Regular", size=10)
-label_tobePaid = customtkinter.CTkLabel(master=root, text="REMAINING",anchor="w",font = font_tobePaid)
-label_tobePaid.grid(row=4, column=1,pady=70,padx=210, sticky="nw")
+font_remainingLabel= customtkinter.CTkFont(family="Poppins Regular", size=10)
+label_remaining = customtkinter.CTkLabel(master=root, text="REMAINING",anchor="w",font = font_remainingLabel)
+label_remaining.grid(row=4, column=1,pady=70,padx=210, sticky="nw")
 root.mainloop()
